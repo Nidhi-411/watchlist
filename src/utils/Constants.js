5caa709d94818d5160d5824fa0844dd95aae8882
Nidhi Yadav <shub01690@gmail.com>
2024-07-29 09:42:09 +0530

    https

--- src/utils/Constants.js
+++ src/utils/Constants.js
@@ -1,7 +1,7 @@
 
  const  Omdb_Api_Key= "163be679";
 
- export const MOVIE_API_URL = "http://www.omdbapi.com/?apikey="+Omdb_Api_Key+"&s=star+wars&page=3";
+ export const MOVIE_API_URL = "https://www.omdbapi.com/?apikey="+Omdb_Api_Key+"&s=star+wars&page=3";
 
  
  // https://www.omdbapi.com/?apikey=163be679&s=star+wars&page=2
